2c9513b7fb2a875ab47bb7d72a50429d0207f95c
Dmytro_Voronov <Dmytro_Voronov@EPUAKYIW1480>
2016-12-23 19:02:35 +0200

    new project

---
+++ Test/src/Test/Controllers/MariaController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Test.Controllers
+{
+    public class MariaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Scott");
+        }
+    }
+
+    public class ScottIndexViewModel
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class ScottController : Controller
+    {
+        public IActionResult Index()
+        {
+            //Get the model from DB
+            var vm = new ScottIndexViewModel()
+            {
+                Age = 29,
+                Name = "Scott"
+            };
+
+            //Do some stuff
+            return View(vm);
+        }
+    }
+}
